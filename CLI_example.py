0b6dbc1d6fcd7bfbb48f7808c8fcc9d731522269
Jonas Emrich <jonas.emrich@gmx.de>
2024-11-23 18:45:34 +0100

    update

--- CLI_example.py
+++ CLI_example.py
@@ -1,14 +1,14 @@
 import subprocess
 
-#### sending a single command
-p = subprocess.run('ls', shell=True, check=True, capture_output=True, encoding='utf-8')
-print(f'Command \"{p.args}\" exited with returncode {p.returncode}, output: \n{p.stdout}')
+# #### sending a single command
+# p = subprocess.run('ls', shell=True, check=True, capture_output=True, encoding='utf-8')
+# print(f'Command \"{p.args}\" exited with returncode {p.returncode}, output: \n{p.stdout}')
 
 
-#### sending multiple commands at once
-commands = "cd test; mkdir test_in_test; ls"
-p = subprocess.run(commands, shell=True, check=True, capture_output=True, encoding='utf-8')
-print(f'Command \"{p.args}\" exited with returncode {p.returncode}, output: \n{p.stdout}')
+# #### sending multiple commands at once
+# commands = "cd test; mkdir test_in_test; ls"
+# p = subprocess.run(commands, shell=True, check=True, capture_output=True, encoding='utf-8')
+# print(f'Command \"{p.args}\" exited with returncode {p.returncode}, output: \n{p.stdout}')
 
 
 #### multiple commands after one another
